3a0294d0c00412ccb92a802279e9e772074f039c
RAHIM <DOSSORAHIM2018@GMAIL.COM>
2023-05-25 15:27:41 +0200

    premier

---
+++ exercices/js/exercice.js
@@ -0,0 +1,39 @@
+// var Prenom = " rahimmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"; //string
+
+
+// var monNumero = 65; // numerique / number
+
+
+// var monNum = "100";
+
+
+// //var nom = izadi; // syntaxe false
+
+
+// var booleanFalse = false; // boolean
+
+
+// var booleanTrue = true; // boolean
+
+// var varNull = null;
+
+
+//alert(Prenom);
+//document.write("A la pause vous aurez des Haribos Pik !");
+//window.prompt("Question: on est quel jour ?", "jour de la semaine");
+ 
+
+
+
+// const PAYS = "france ";
+// PAYS = "italie "
+// document.write(PAYS);
+
+
+
+
+
+var monPrenom = prompt();
+document.write(monPrenom);
+console.log(monPrenom);
+
---
+++ initiation/js/exercice1.js
@@ -0,0 +1,10 @@
+var nb1 = prompt("");
+var nb2 = 7;
+nb1 = parseInt(nb1);
+console.log(typeof nb1);
+var resultat = nb1 % nb2;
+var nb3 = nb1 % nb2;
+nb3 += nb2;
+window.alert(resultat);
+document.write(resultat);
+console.log(resultat);
