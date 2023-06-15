093b2247389f61e069835d412eec5435665f9008
RAHIM <DOSSORAHIM2018@GMAIL.COM>
2023-06-15 16:56:03 +0200

    5 commit

--- exercices/js/conversion.js
+++ exercices/js/conversion.js
@@ -9,6 +9,89 @@
 
 
 
-function affichagePrenoms(chaineCaractere) {
-  document.write(chaineCaractere);
-}
+// function affichagePrenoms(chaineCaractere) {
+//   document.write(chaineCaractere);
+// }
+
+// vara = 1;
+// varb = 5;
+// console.log("----------Avant Inversion------------");
+// console.log("A : " + a);
+// console.log("B : " + b);
+// tmp = a;
+// a = b;
+// b = tmp;
+// console.log("----------Après Inversion------------");
+// console.log("A : " + a);
+// console.log("B : " + b);
+
+
+
+
+
+// ***************************** permuter deux variable ***************************
+// var nombre1= 5;
+// var nombre2 = 3;
+// var nombre3 =0 ;
+// console.log("----------Avant Inversion------------");
+// console.log("nombre1 : " + nombre1);
+// console.log("nombre2 : " + nombre2);
+// nombre3 = nombre1;
+// nombre1 = nombre2;
+// nombre2 = nombre3;
+// console.log("----------Après Inversion------------");
+// console.log("nombre1 : " + nombre1);
+// console.log("nombre2 : " + nombre2);
+
+
+// var num = prompt();
+// while (num <= 3 || num >= 120 || isNaN(num)) {
+//   num = prompt();
+
+
+// }
+// var majoriteFR = 18;
+// // function affichAge(age)
+
+
+
+// var prenom = [] ;
+// var adjectif =[] ;
+
+
+
+// while (prenom.length <=12) {
+//   prenomRAa = prompt("saisie un prenom");
+//   prenom.push(prenomRAa);
+//   document.write(prenomRAa);
+
+// }
+
+// while (adjectif.length <= 12) {
+//   adjectifjpg = prompt("saisie un adjectif");
+//   adjectif.push( adjectifjpg);
+//   document.write(adjectifjpg);
+// }
+
+
+// function tableau (prenom, adjectif)
+// var tab = prenom + adjectif
+// return tab
+
+
+
+// while (true) {
+//   var age = prompt("Indiquez votre âge :");
+  
+//   if (!isNaN(age) && age !== "") {
+//     age = parseInt(age);
+//     if (age <= 3 || age >= 120) {
+//       alert("La valeur est valide.");
+//       break;
+//     } else {
+//       alert("La valeur doit être inférieure ou égale à 3, supérieure ou égale à 120.");
+//     }
+//   } else {
+//     alert("La valeur doit être un nombre entier.");
+//   }
+// }
--- exercices/js/exercice1.js
+++ exercices/js/exercice1.js
@@ -1,10 +1,22 @@
-var nb1 = prompt("");
-var nb2 = 7;
-nb1 = parseInt(nb1);
-console.log(typeof nb1);
-var resultat = nb1 % nb2;
-var nb3 = nb1 % nb2;
-nb3 += nb2;
-window.alert(resultat);
-document.write(resultat);
-console.log(resultat);
+// var nb1 = prompt("");
+// var nb2 = 7;
+// nb1 = parseInt(nb1);
+// console.log(typeof nb1);
+// var resultat = nb1 % nb2;
+// var nb3 = nb1 % nb2;
+// nb3 += nb2;
+// window.alert(resultat);
+// document.write(resultat);
+// console.log(resultat);
+
+
+function fillArray(num);
+var addElement = []
+for (var i = 0; i <= num; i++) {
+        var nb1 = prompt();
+
+}
+
+
+
+
